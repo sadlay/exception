a0f58edebd84b4fd81a3ef1777c1236b0dc28230
zhaogengsheng <zhaogengsheng@gmail.com>
2019-08-09 18:15:52 +0800

    封装请求data
    (cherry picked from commit 8616f6cf5390e12c63555c386487267d86491056)
    
    # Conflicts:
    #	src/main/java/cn/layanan/exception/core/config/SpringMvcConfig.java

--- src/main/java/cn/layanan/exception/core/config/SpringMvcConfig.java
+++ src/main/java/cn/layanan/exception/core/config/SpringMvcConfig.java
@@ -3,12 +3,10 @@ package cn.layanan.exception.core.config;
 import cn.layanan.exception.core.bind.JwtTokenArgumentResolver;
 import cn.layanan.exception.core.bind.PropertyNamingStrategyArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,39 +28,16 @@ public class SpringMvcConfig extends WebMvcConfigurationSupport {
     @Autowired
     private HttpMessageConverter requestDataMessageConvert;
 
-
+    /**/
     @Override
     protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         super.addArgumentResolvers(argumentResolvers);
         argumentResolvers.add(jwtTokenArgumentResolver);
         argumentResolvers.add(new PropertyNamingStrategyArgumentResolver(true));
-/*        List<HttpMessageConverter<?>> converters = new ArrayList();
-        converters.add(requestDataMessageConvert);
-        argumentResolvers.add(new RequestDataTypeMethodProcessor(converters));*/
-    }
-
-/*
-    */
-/**
-     * 配置请求处理器适配器
-     *//*
-
-    //@Bean
-    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
-        RequestMappingHandlerAdapter original = super.requestMappingHandlerAdapter();
         List<HttpMessageConverter<?>> converters = new ArrayList();
         converters.add(requestDataMessageConvert);
-        List<HandlerMethodArgumentResolver> argumentResolvers = new ArrayList();
         argumentResolvers.add(new RequestDataTypeMethodProcessor(converters));
-        original.setCustomArgumentResolvers(argumentResolvers);
- */
-/*       List<HttpMessageConverter<?>> fastjson = new ArrayList();
-        fastjson.add(fastJsonHttpMessageConverters);
-        original.setMessageConverters(fastjson);*//*
-
-        return original;
     }
-*/
 
     @Override
     protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
